6ae13a905a48efbe6e8b857880a4f0f749c1a56d
chaitu111 <koripellachaitanya11@gmail.com>
2018-09-10 00:08:47 +0530

    test

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { 
+	StyleSheet, 
+	Text, 
+	View 
+} 
+from 'react-native';
+
+
+// importing  Library 
+import { Container, Content, Icon, Header, Body } from 'native-base'
+import { createDrawerNavigator } from 'react-navigation'
+
+// importing  Custom Screens
+import HomeScreen from './pages/HomeScreen';
+import SettingScreen from './pages/SettingScreen';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <MyNav />
+    );
+  }
+}
+
+
+const MyNav = createDrawerNavigator ({
+
+  Home: { screen: HomeScreen },
+  Setting: { screen: SettingScreen }
+})
+
+export default App;
+
+const styles = StyleSheet.create({
+  
+});
+
